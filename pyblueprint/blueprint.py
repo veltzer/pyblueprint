f32e7a2b3f1db504982a30a3676ec4c79723893f
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-03 09:37:37 +0200

    more stuff

--- pyblueprint/blueprint.py
+++ pyblueprint/blueprint.py
@@ -1,3 +1,20 @@
 """
 This is the module of pyblueprint
 """
+import svgwrite
+import svgwrite.text
+import svgwrite.shapes
+
+
+class BluePrint:
+    def __init__(self, filename: str):
+        self.dwg = svgwrite.Drawing(filename=filename)
+
+    def text(self, text: str):
+        self.dwg.add(svgwrite.text.Text(text=text))
+
+    def square(self):
+        self.dwg.add(svgwrite.shapes.Rect())
+
+    def save(self) -> None:
+        self.dwg.save()
