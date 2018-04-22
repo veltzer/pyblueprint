ca0c60a164299a3b35d2a58ded9803fbb8e13f48
Mark Veltzer <mark.veltzer@gmail.com>
2018-04-22 17:38:55 +0300

    move files to tmp

--- examples/empty.py
+++ examples/empty.py
@@ -1,12 +1,12 @@
 import pyblueprint.blueprint
 
-b = pyblueprint.blueprint.BluePrint(filename="empty.svg")
+b = pyblueprint.blueprint.BluePrint(filename="/tmp/empty.svg")
 b.save()
 
-b = pyblueprint.blueprint.BluePrint(filename="hello.svg")
+b = pyblueprint.blueprint.BluePrint(filename="/tmp/hello.svg")
 b.text("Hello")
 b.save()
 
-b = pyblueprint.blueprint.BluePrint(filename="square.svg")
+b = pyblueprint.blueprint.BluePrint(filename="/tmp/square.svg")
 b.square()
 b.save()
