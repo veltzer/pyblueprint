f87caf9c8c59a2d5f4e124b5c1add55606d9eb9b
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-27 14:53:58 +0300

    better git config

--- examples/empty.py
+++ examples/empty.py
@@ -1,12 +1,12 @@
 import pyblueprint.blueprint
 
-b = pyblueprint.blueprint.BluePrint(filename="/tmp/empty.svg")
-b.save()
+b = pyblueprint.blueprint.BluePrint()
+b.save(filename="/tmp/empty.svg")
 
-b = pyblueprint.blueprint.BluePrint(filename="/tmp/hello.svg")
+b = pyblueprint.blueprint.BluePrint()
 b.text("Hello")
-b.save()
+b.save(filename="/tmp/hello.svg")
 
-b = pyblueprint.blueprint.BluePrint(filename="/tmp/square.svg")
+b = pyblueprint.blueprint.BluePrint()
 b.square()
-b.save()
+b.save(filename="/tmp/square.svg")
